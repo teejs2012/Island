46d8cc19ac2c421f90b9e86b38d4b6289e602506
jiasheng <teejs2012@gmail.com>
2018-07-20 12:14:50 +0800

    device tracking try

--- Assets/Scripts/GravityTest/CustomGravity.cs
+++ Assets/Scripts/GravityTest/CustomGravity.cs
@@ -7,8 +7,10 @@ public class CustomGravity : MonoBehaviour {
 	public float gravityForce = 380;
     bool gyroEnabled = false;
     Gyroscope gyro;
+    //[SerializeField]
+    //Transform anchor;
     [SerializeField]
-    Transform anchor;
+    Transform imageTarget;
     [SerializeField]
     Transform vuforiaCam;
 
@@ -17,9 +19,13 @@ public class CustomGravity : MonoBehaviour {
     public Text arCamY;
     public Text arCamZ;
 
-    public Text deviceX;
-    public Text deviceY;
-    public Text deviceZ;
+    public Text imageTargetX;
+    public Text imageTargetY;
+    public Text imageTargetZ;
+
+    //public Text deviceX;
+    //public Text deviceY;
+    //public Text deviceZ;
     // Use this for initialization
     void Start () {
         //CameraDevice.Instance.StopAutoFocus();
@@ -32,43 +38,44 @@ public class CustomGravity : MonoBehaviour {
         //	Debug.Log ("PORTRAIT");
         //}
 
-        if (SystemInfo.supportsGyroscope)
-        {
-            gyro = Input.gyro;
-            gyro.enabled = true;
-            gyroEnabled = true;
-        }
+        //if (SystemInfo.supportsGyroscope)
+        //{
+        //    gyro = Input.gyro;
+        //    gyro.enabled = true;
+        //    gyroEnabled = true;
+        //}
 	}
 	
-	void FixedUpdate () {
-        if (!gyroEnabled)
-        {
-            return;
-        }
-
-        
-        anchor.transform.rotation = gyro.attitude;
-        deviceX.text = anchor.transform.eulerAngles.x.ToString();
-        deviceY.text = anchor.transform.eulerAngles.y.ToString();
-        deviceZ.text = anchor.transform.eulerAngles.z.ToString();
-
-        arCamX.text = vuforiaCam.transform.eulerAngles.x.ToString();
-        arCamY.text = vuforiaCam.transform.eulerAngles.y.ToString();
-        arCamZ.text = vuforiaCam.transform.eulerAngles.z.ToString();
-
-
-        Vector3 localDownVector = anchor.InverseTransformDirection(Vector3.down);
-        Vector3 gravityDir = vuforiaCam.TransformDirection(localDownVector);
-        //CameraDevice.Instance.StopAutoFocus();
-        /*if (Screen.orientation == ScreenOrientation.LandscapeLeft) {
-			Physics.gravity = new Vector3 (Input.acceleration.y * grav , -Input.acceleration.x * grav , -Input.acceleration.z * grav);
-		} else if (Screen.orientation == ScreenOrientation.LandscapeRight ) {
-			Physics.gravity = new Vector3 (-Input.acceleration.y * grav , Input.acceleration.x * grav , -Input.acceleration.z * grav);
-		} else if (Screen.orientation == ScreenOrientation.Portrait ) {*/
-        Physics.gravity = gravityDir * gravityForce;
+	void Update () {
+        //if (!gyroEnabled)
+        //{
+        //    return;
         //}
 
+        
+        //anchor.transform.rotation = gyro.attitude;
+        //deviceX.text = anchor.transform.eulerAngles.x.ToString();
+        //deviceY.text = anchor.transform.eulerAngles.y.ToString();
+        //deviceZ.text = anchor.transform.eulerAngles.z.ToString();
+
+        arCamX.text = vuforiaCam.eulerAngles.x.ToString();
+        arCamY.text = vuforiaCam.eulerAngles.y.ToString();
+        arCamZ.text = vuforiaCam.eulerAngles.z.ToString();
+
+        imageTargetX.text = imageTarget.eulerAngles.x.ToString();
+        imageTargetY.text = imageTarget.eulerAngles.y.ToString();
+        imageTargetZ.text = imageTarget.eulerAngles.z.ToString();
 
 
+        //      Vector3 localDownVector = anchor.InverseTransformDirection(Vector3.down);
+        //      Vector3 gravityDir = vuforiaCam.TransformDirection(localDownVector);
+        //      //CameraDevice.Instance.StopAutoFocus();
+        //      /*if (Screen.orientation == ScreenOrientation.LandscapeLeft) {
+        //	Physics.gravity = new Vector3 (Input.acceleration.y * grav , -Input.acceleration.x * grav , -Input.acceleration.z * grav);
+        //} else if (Screen.orientation == ScreenOrientation.LandscapeRight ) {
+        //	Physics.gravity = new Vector3 (-Input.acceleration.y * grav , Input.acceleration.x * grav , -Input.acceleration.z * grav);
+        //} else if (Screen.orientation == ScreenOrientation.Portrait ) {*/
+        //      Physics.gravity = gravityDir * gravityForce;
+        //      //}
     }
 }
