7fa2f673eba419033c6196b2ddb5462ca5b1e3ec
jiasheng <teejs2012@gmail.com>
2018-08-16 21:02:30 +0800

    add cover scene

---
+++ Assets/Scripts/CoverContoller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+
+public class CoverContoller : MonoBehaviour {
+
+    [SerializeField]
+    GameObject loadingBarParent;
+    [SerializeField]
+    Image loadingBar;  
+    [SerializeField]
+    Button startButton;
+
+    void Awake()
+    {
+        Assert.IsTrue(startButton != null);
+        Assert.IsTrue(loadingBar != null);
+        startButton.onClick.AddListener(()=> { LoadScene(); });
+    }
+
+    void LoadScene()
+    {
+        var loadOperation = SceneManager.LoadSceneAsync("Main");
+        loadingBarParent.SetActive(true);
+        StartCoroutine(UpdateLoadingBar(loadOperation));
+    }
+
+    IEnumerator UpdateLoadingBar(AsyncOperation operation)
+    {
+        while (!operation.isDone)
+        {
+            loadingBar.fillAmount = operation.progress;
+            yield return null;
+        }
+    }
+}
